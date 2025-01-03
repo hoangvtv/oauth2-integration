8ad25a5614c173bb44e6143a77504c85af800d84
hoangvtv <phamtanhoangvtv@gmail.com>
2025-01-03 22:24:40 +0700

    feat: build API exchange token with google

---
+++ identity_service/src/main/java/com/phamtanhoang/identity_service/dto/response/ExchangeTokenResponse.java
@@ -0,0 +1,20 @@
+package com.phamtanhoang.identity_service.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class ExchangeTokenResponse {
+    String accessToken;
+    String expiresIn;
+    String refreshToken;
+    String scope;
+    String tokenType;
+}
