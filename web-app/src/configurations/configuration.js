302b3a1a7c16089fda2bc3188a347ae5f6086a84
hoangvtv <phamtanhoangvtv@gmail.com>
2025-01-04 14:48:54 +0700

    feat: build webapp

---
+++ web-app/src/configurations/configuration.js
@@ -0,0 +1,5 @@
+export const OAuthConfig = {
+  clientId: "209323883719-b23g57808hagjn1nu1135o2cev193psm.apps.googleusercontent.com",
+  redirectUri: "http://localhost:3000/authenticate",
+  authUri: "https://accounts.google.com/o/oauth2/auth",
+};
